c3d66e0df3007ae9f2917f0ae2e5f4974f82d579
kor-Chipmunk <rhj4862@gmail.com>
2020-10-06 13:51:46 +0900

    [Issue-09] Update: UserLoginAPIView Serializer style refactored

--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, status
+from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -8,13 +9,15 @@ from .models import User
 class UserCreateAPIView(generics.CreateAPIView):
     serializer_class = UserCreateSerializer
     queryset = User.object.all()
+    renderer_classes = [JSONRenderer]
 
 class UserLoginAPIView(APIView):
     serializer_class = UserLoginSerializer
+    renderer_classes = [JSONRenderer]
 
     def post(self, request, *args, **kwargs):
         data = request.data
-        serializer = UserLoginSerializer(data=data)
+        serializer = self.serializer_class(data=data)
 
         if serializer.is_valid(raise_exception=True):
             new_data = serializer.data
