218c73a9902824ac21a9133221329df3afff32fe
kor-Chipmunk <rhj4862@gmail.com>
2020-10-06 10:38:12 +0900

    [issue-04] Added: Question & Answer model added

---
+++ introduce/apps.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+
+class IntroduceConfig(AppConfig):
+    name = 'introduce'
---
+++ introduce/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+from user.models import User
+
+
+class Question(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'intro_questions'
+
+class Answer(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             verbose_name="답변을 한 유저")
+
+    question = models.ForeignKey(Question,
+                                 on_delete=models.SET_NULL,
+                                 verbose_name="답변한 질문")
+
+    class Meta:
+        db_table = 'intro_answers'
