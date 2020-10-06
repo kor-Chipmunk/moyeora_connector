17c2df5a5838387687393f0a4a09522617cde568
kor-Chipmunk <rhj4862@gmail.com>
2020-10-06 17:17:15 +0900

    [Issue-12] Fix: Introduce router duplicated error fixed

--- introduce/urls.py
+++ introduce/urls.py
@@ -3,9 +3,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = format_suffix_patterns([
-    path('questions', views.get_questions),
-    path('questions', views.post_questions),
-    path('questions/<int:question_id>', views.get_question),
-    path('questions/<int:question_id>', views.put_questions),
-    path('questions/<int:question_id>', views.delete_questions),
+    path('questions', views.create_list_questions),
+    path('questions/<int:question_id>', views.retrieve_update_destory_question),
+
+    path('answers', views.list_create_answers),
+    path('answers/<int:answer_id>', views.get_answer),
 ])
--- introduce/views.py
+++ introduce/views.py
@@ -6,59 +6,71 @@ from rest_framework.response import Response
 from .models import Question, Answer
 from .serializers import QuestionSerializer, AnswerSerializer
 
+@api_view(["GET", "POST"])
+@permission_classes([IsAuthenticated])
+def create_list_questions(request):
+    if request.method == "GET":
+        questions = Question.objects.all()
+        serializer = QuestionSerializer(questions, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "POST":
+        serializer = QuestionSerializer(data=request.data)
+
+        if (serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET", "PUT", "DELETE"])
+@permission_classes([IsAuthenticated])
+def retrieve_update_destory_question(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = QuestionSerializer(question)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "PUT":
+        serializer = QuestionSerializer(question, data=request.data)
+
+        if (serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        question.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(["GET", "POST"])
+@permission_classes([IsAuthenticated])
+def list_create_answers(request):
+    if request.method == "GET":
+        answers = Answer.objects.filter(user=request.user)
+        serializer = AnswerSerializer(answers, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "POST":
+        serializer = AnswerSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
-def get_questions(request):
-    questions = Question.objects.all()
-    serializer = QuestionSerializer(questions, many=True)
+def get_answer(request, answer_id):
+    try:
+        answer = Answer.objects.get(pk=answer_id)
+    except Answer.DoesNotExist:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
+    if answer.user != request.user:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+    serializer = AnswerSerializer(answer)
     return Response(serializer.data, status=status.HTTP_200_OK)
-
-@api_view(["GET"])
-@permission_classes([IsAuthenticated])
-def get_question(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    serializer = QuestionSerializer(question)
-
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-@api_view(["POST"])
-@permission_classes([IsAdminUser])
-def post_questions(request):
-    serializer = QuestionSerializer(data=request.data)
-
-    if (serializer.is_valid()):
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(["PUT"])
-@permission_classes([IsAdminUser])
-def put_questions(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    serializer = QuestionSerializer(question, data=request.data)
-
-    if (serializer.is_valid()):
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(["DELETE"])
-@permission_classes([IsAdminUser])
-def delete_questions(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    question.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
