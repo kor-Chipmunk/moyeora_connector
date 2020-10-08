36cbe659e08916552f2086a692c19da63581a660
kor-Chipmunk <rhj4862@gmail.com>
2020-10-08 19:04:53 +0900

    [Issue-18] Added: django_seed added, redis cache setting added

--- moyeora_connector/settings.py
+++ moyeora_connector/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'debug_toolbar',
+    'django_seed',
     'user',
     'introduce',
     'music',
@@ -159,6 +160,16 @@ LOGGING = {
         'django.db.backends': {
             'handlers': ['console'],
             'level': "DEBUG"
+        }
+    }
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
     }
 }
