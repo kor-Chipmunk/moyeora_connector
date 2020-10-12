f45a111ec2a3951d0426781039aa2d7e6d3b6f73
kor-Chipmunk <rhj4862@gmail.com>
2020-10-12 10:49:37 +0900

    [Issue-25] Added: Introduce domain added common responses

--- introduce/views.py
+++ introduce/views.py
@@ -3,6 +3,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from moyeora_connector.responses import CommonResponse
+from .exceptions import *
 from .models import Question, Answer
 from .serializers import QuestionSerializer, AnswerCreationSerializer, AnswerDetailSerializer, \
     AnswerOnlyContentSerializer
@@ -15,17 +17,17 @@ def create_list_questions(request):
         questions = Question.objects.all()
         serializer = QuestionSerializer(questions, many=True)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return CommonResponse('INTRODUCE_QUESTION_LIST', status.HTTP_200_OK, serializer.data, '성공적으로 질문을 조회했습니다.')
     elif request.method == "POST":
         if not request.user.is_admin:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+            return INTRO_UNAUTHORIZE_ADMIN
 
         serializer = QuestionSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return CommonResponse('INTRODUCE_QUESTION_CREATED', status.HTTP_201_CREATED, serializer.data, '성공적으로 질문을 등록했습니다.')
+        return INTRO_QUESTION_INVALID
 
 @api_view(["GET", "PUT", "DELETE"])
 @permission_classes([IsAuthenticated])
@@ -33,7 +35,7 @@ def retrieve_update_destory_question(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        return INTRO_QUESTION_NOTFOUND
 
     if request.method == "GET":
         serializer = QuestionSerializer(question)
@@ -41,7 +43,7 @@ def retrieve_update_destory_question(request, question_id):
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == "PUT":
         if not request.user.is_admin:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+            return INTRO_UNAUTHORIZE_ADMIN
 
         serializer = QuestionSerializer(question, data=request.data, partial=True)
 
@@ -50,14 +52,15 @@ def retrieve_update_destory_question(request, question_id):
                 return Response(status=status.HTTP_401_UNAUTHORIZED)
 
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+            return CommonResponse('INTRO_QUESTION_UPDATED', status.HTTP_201_CREATED, serializer.data, '성공적으로 질문이 수정됐습니다.')
+        return INTRO_QUESTION_INVALID
     elif request.method == "DELETE":
         if not request.user.is_admin:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+            return INTRO_UNAUTHORIZE_ADMIN
 
         question.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return CommonResponse('INTRO_QUESTION_DELETED', status.HTTP_204_NO_CONTENT, True, '성공적으로 질문이 삭제됐습니다.')
 
 @api_view(["GET", "POST"])
 @permission_classes([IsAuthenticated])
@@ -74,7 +77,7 @@ def list_create_answers(request):
 
         serializer = AnswerDetailSerializer(answers, many=True)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return CommonResponse('INTRO_ANSWER_LIST', status.HTTP_200_OK, serializer.data, '성공적으로 답변 목록을 조회했습니다.')
     elif request.method == "POST":
         request.data['user'] = request.user.pk
         serializer = AnswerCreationSerializer(data=request.data)
@@ -87,8 +90,8 @@ def list_create_answers(request):
 
             serializer = AnswerOnlyContentSerializer(serializer.data)
 
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return CommonResponse('INTRO_ANSWER_CREATED', status.HTTP_201_CREATED, serializer.data, '성공적으로 답변이 작성됐습니다.')
+        return INTRO_ANSWER_INVALID
 
 @api_view(["GET", "PUT", "DELETE"])
 @permission_classes([IsAuthenticated])
@@ -96,16 +99,16 @@ def retrieve_update_destory_answer(request, answer_id):
     try:
         answer = Answer.objects.get(pk=answer_id)
     except Answer.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        return INTRO_ANSWER_NOTFOUND
 
     if not request.user.is_admin:
         if request.user != answer.user:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+            return INTRO_ANSWER_UNAUTHORIZE
 
     if request.method == "GET":
         serializer = AnswerDetailSerializer(answer)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return CommonResponse('INTRO_ANSWER_LIST', status.HTTP_200_OK, serializer.data, '성공적으로 답변 목록을 작성했습니다.')
     elif request.method == "PUT":
         request.data['user'] = request.user.pk
 
@@ -113,8 +116,8 @@ def retrieve_update_destory_answer(request, answer_id):
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return CommonResponse('INTRO_ANSWER_CREATED', status.HTTP_201_CREATED, serializer.data, '성공적으로 답변을 작성했습니다.')
+        return INTRO_ANSWER_INVALID
     elif request.method == "DELETE":
         answer.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return CommonResponse('INTRO_ANSWER_DELETED', status.HTTP_204_NO_CONTENT, True, '성공적으로 답변을 삭제했습니다.')
